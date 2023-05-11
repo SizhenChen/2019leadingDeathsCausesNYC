6c2b68ea1da4963b49326e3700b250d52bb50183
SizhenChen <chens656@newschool.edu>
2023-05-10 21:39:07 -0400

    up

--- script.js
+++ script.js
@@ -1,4 +1,136 @@
+let dropArray = [
+  {
+    "fullName": "Diseases of Heart (I00-I09, I11, I13, I20-I51)",
+    "name": "Diseases of Heart",
+    "image": "imgs/心脏病.png",
+    "comment": `Heart disease remains the top cause of death. 
+                Afterall, you die when your heart stops beating, right? <br>
+                <br>
+                My chest is empty so I wouldn't know how that feels, and I
+                don't know how love can raise your heart beat. <br>
+                <br>
+                Hearts are very important for humans, huh.`
+  },
 
+  {
+    "fullName": "Malignant Neoplasms (Cancer: C00-C97)",
+    "name": "Malignant Neoplasm (Cancer)",
+    "image": "imgs/癌症.png",
+    "comment": `Cancer is one of the best employees, but I hate it because more death just means more work for me. <br>
+               <br>
+               But seriously, why do I call these diseases "employees" when I am cleaning their businesses. <br>
+               <br>
+               They should be paying me!`
+  },
+
+  {
+    "fullName": "Mental and Behavioral Disorders due to Accidental Poisoning and Other Psychoactive Substance Use (F11-F16, F18-F19, X40-X42, X44)",
+    "name": "Mental and Behavioral Disorders due to Accidental Poisoning",
+    "image": "imgs/毒.png",
+    "comment": `Mental and behavioral disorders due to accidental poisoning? I don't even know what that is!<br>
+              <br>
+              Cancer's autographs are piled next to the trashcan by the way.`
+  },
+
+  {
+    "fullName": "Diabetes Mellitus (E10-E14)",
+    "name": "Diabetes Mellitus",
+    "image": "imgs/糖尿病.png",
+    "comment": `Ah, diabetes, you thought one of the most common diseases won't be a life threat. <br>
+               <br>
+               I am definitely quitting carbs. Wait, I don't even have a body.`
+  },
+
+  {
+    "fullName": "Influenza (Flu) and Pneumonia (J09-J18)",
+    "name": "Influenza (Flu) and Pneumonia",
+    "image": "imgs/流感.png",
+    "comment": `It is the post-pandemic era now so people might be super familiar to this disease right now. <br>
+               <br>
+               This guy is getting cocky, which is bad for our workplace. It's good to let humans suppress it a little.`
+  },
+
+  {
+    "fullName": "Cerebrovascular Disease (Stroke: I60-I69)",
+    "name": "Cerebrovascular Disease (Stroke)",
+    "image": "imgs/脑血管疾病.png",
+    "comment": `For elders, having a stroke once is unbelievably detrimental. Once my grandpa had a stroke and 
+               his brain functions never recovered...<br>
+               <br>
+               Hold on, whose memory is this?`
+  },
+
+
+  {
+    "fullName": "Accidents Except Drug Poisoning (V01-X39, X43, X45-X59, Y85-Y86)",
+    "name": "Accidents Except Drug Poisoning",
+    "image": "imgs/意外.png",
+    "comment": `This is a cause of death that is not under my surveillance. It's not a surprise that most of them are products of modernization.<br>
+              <br>
+              For me, it just means more work.`
+  },
+
+  {
+    "fullName": "Chronic Liver Disease and Cirrhosis (K70, K73-K74)",
+    "name": "Chronic Liver Disease and Cirrhosis",
+    "image": "imgs/肝硬化.png",
+    "comment": `Ugh, I googled this guy's portfolio and those are the grossest images I've ever seen ...
+               not really, I'm literally THE DEATH and I've seen enough, but you certainly should quit drinking alcohol if it does that to your liver.`
+  },
+
+  {
+    "fullName": "Chronic Lower Respiratory Diseases (J40-J47)",
+    "name": "Chronic Lower Respiratory Diseases",
+    "image": "imgs/慢性阻塞肺病.png",
+    "comment": `Yeah, smoking can actually kill you. It is a known fact but some people are just in denial ... or do they wish to be killed?<br>
+               <br>
+               Since you can't ban it, why don't you just be the clever guy and let the others die for it instead?`
+  },
+
+  {
+    "fullName": "Essential Hypertension and Renal Diseases (I10, I12)",
+    "name": "Essential Hypertension and Renal Disease",
+    "image": "imgs/高血压性肾病.png",
+    "comment": `You know what, for someone who is not a doctor, this is a learning experience for me. This is the first time I've even heard of this disease!`
+  },
+
+  {
+    "fullName": "Intentional Self-Harm (Suicide: U03, X60-X84, Y87.0)",
+    "name": "Intentional Self-Harm",
+    "image": "imgs/自残.png",
+    "comment": `Ugh, I hate this guy. I have issues dealing with it. <br>
+               <br>
+               It just keeps neglecting all the good things I've done and nagging about my failures ... <br> 
+               <br>
+               No, I mean you deserve to be loved!`
+  },
+
+  {
+    "fullName": "Alzheimer's Disease (G30)",
+    "name": "Alzheimer's Disease",
+    "image": "imgs/阿兹海默.png",
+    "comment": `A lot of books and films were based on this disease, but nothing can compare to its reality. 
+               Is it crueler to have me take a life away, or watch them live but no better than dead? Who knows.`
+  },
+
+  {
+    "fullName": "Assault (Homicide: U01-U02, Y87.1, X85-Y09)",
+    "name": "Assault (Homicide)",
+    "image": "imgs/谋杀.png",
+    "comment": `People hate and fear me, but to my defense, they are the major cause of their species' demise. <br>
+              <br>
+               I am always amazed of their inventions on how to torture each other.`
+  },
+
+  {
+    "fullName": "All Other Causes",
+    "name": "All Other Causes",
+    "image": "imgs/其它原因.png",
+    "comment": `Although there are tons of causes we haven't mentioned, they also deserve a place on this site.<br>
+               <br>
+               So yeah, this is like a consolation prize for them. Good job losers!`
+  },
+]
 
 
 var sketch1 = function (f1) {
@@ -71,7 +203,6 @@ console.log(diseasesDrop.value)
 
 function getData() {
   let Name = document.getElementById("Name")
-  let dropValue = diseasesDrop.value
   Name.innerHTML = `<div>${diseasesDrop.options[this.selectedIndex].text}</div>`
   for (let i = 0; i < dropArray.length; i++) {
     let Image = document.getElementById("Image")
@@ -84,147 +215,6 @@ function getData() {
   }
 }
 
-let dropArray = [
-  {
-    "fullName": "Diseases of Heart (I00-I09, I11, I13, I20-I51)",
-    "name": "Diseases of Heart",
-    "image": "imgs/心脏病.png",
-    "comment": `The heart disease remains the top cause of death. 
-                Afterall, you die when your heart stop beating, right? <br>
-                <br>
-                My chest is empty so I wouldn't know how that feels, and I
-                don't know how love can raises your heart beat. <br>
-                <br>
-                Hearts are very important for humans, huh.`
-  },
-
-  {
-    "fullName": "Malignant Neoplasms (Cancer: C00-C97)",
-    "name": "Malignant Neoplasm (Cancer)",
-    "image": "imgs/癌症.png",
-    "comment": `Cancer is one of the best employees, but I hate it because more death just mean more work for me. <br>
-               <br>
-               But seriously, why do I call these diseases employee when I am cleaning their businesses. <br>
-               <br>
-               They should be paying me.`
-  },
-
-  {
-    "fullName": "Mental and Behavioral Disorders due to Accidental Poisoning and Other Psychoactive Substance Use (F11-F16, F18-F19, X40-X42, X44)",
-    "name": "Mental and Behavioral Disorders due to Accidental Poisoning",
-    "image": "imgs/毒.png",
-    "comment": `Mental and behavioral disorders due to accidental poisoning? I don't even know what that is!<br>
-              <br>
-              Cancer's autographs are piled next to the trashcan by the way.`
-  },
-
-  {
-    "fullName": "Diabetes Mellitus (E10-E14)",
-    "name": "Diabetes Mellitus",
-    "image": "imgs/糖尿病.png",
-    "comment": `Ah, diabetes, you thought one of the most common disease won't be a life threat. <br>
-               <br>
-               I am definitely quiting carbs. Wait, I don't even have a body.`
-  },
-
-  {
-    "fullName": "Influenza (Flu) and Pneumonia (J09-J18)",
-    "name": "Influenza (Flu) and Pneumonia",
-    "image": "imgs/流感.png",
-    "comment": `It is the post-pandemic era now so people might be super familiar to this disease right now. <br>
-               <br>
-               This guy is getting cocky, which is bad for our workplace. It's good to let humans suppress it a little.`
-  },
-
-  {
-    "fullName": "Cerebrovascular Disease (Stroke: I60-I69)",
-    "name": "Cerebrovascular Disease (Stroke)",
-    "image": "imgs/脑血管疾病.png",
-    "comment": `For elders, having a stroke once is unbelievingly detrimental. Once my grandpa had a stroke and 
-               his brain functions never recovered...<br>
-               <br>
-               Hold on, whose memory is this?`
-  },
-
-
-  {
-    "fullName": "Accidents Except Drug Poisoning (V01-X39, X43, X45-X59, Y85-Y86)",
-    "name": "Accidents Except Drug Poisoning",
-    "image": "imgs/意外.png",
-    "comment": `This is a cause of death that is not under my surveillance. It's not a surprise that most of them are products of modernization.<br>
-              <br>
-              For me, it just means more work.`
-  },
-
-  {
-    "fullName": "Chronic Liver Disease and Cirrhosis (K70, K73-K74)",
-    "name": "Chronic Liver Disease and Cirrhosis",
-    "image": "imgs/肝硬化.png",
-    "comment": `Ugh, I googled this guy's portfolio and those are the grossest images I've ever seen ...
-               not really, I'm death for god sake, but you certainly should quit drinking alcohol if it does that to your liver.`
-  },
-
-  {
-    "fullName": "Chronic Lower Respiratory Diseases (J40-J47)",
-    "name": "Chronic Lower Respiratory Diseases",
-    "image": "imgs/慢性阻塞肺病.png",
-    "comment": `Yeah, smoking can actually kill you. It is a known fact but people are just in denial.<br>
-               <br>
-               Since you can't ban it, why don't you just be the clever guy and let the others die instead?`
-  },
-
-  {
-    "fullName": "Essential Hypertension and Renal Diseases (I10, I12)",
-    "name": "Essential Hypertension and Renal Disease",
-    "image": "imgs/高血压性肾病.png",
-    "comment": `You know what, for someone who is not a doctor, this is a learning experience for me. This is the first time I've even heard of this disease!`
-  },
-
-  {
-    "fullName": "Intentional Self-Harm (Suicide: U03, X60-X84, Y87.0)",
-    "name": "Intentional Self-Harm",
-    "image": "imgs/自残.png",
-    "comment": `Ugh, I hate this guy. I have issues dealing with it. <br>
-               <br>
-               It just keep neglecting all the good things I've done and nagging about my failures ... <br> 
-               <br>
-               No, I mean you deserve to be loved!`
-  },
-
-  {
-    "fullName": "Alzheimer's Disease (G30)",
-    "name": "Alzheimer's Disease",
-    "image": "imgs/阿兹海默.png",
-    "comment": `A lot of books and films were based on this disease. Is it crueler to have me take a life away, or watch them live but no better than dead? Who knows.`
-  },
-
-  {
-    "fullName": "Assault (Homicide: U01-U02, Y87.1, X85-Y09)",
-    "name": "Assault (Homicide)",
-    "image": "imgs/谋杀.png",
-    "comment": `People hate and fear me, but to my defence, they are the major cause of their species' demise. <br>
-              <br>
-               I am always amazed of their inventions on how to torture each other.`
-  },
-
-  {
-    "fullName": "All Other Causes",
-    "name": "All Other Causes",
-    "image": "imgs/其它原因.png",
-    "comment": `Although there are tons of causes we haven't mentioned, they also deserve a place on this site.<br>
-               <br>
-               So yeah, this is like a consolation prize for them. Good job losers!`
-  },
-]
-
-// for (let i = 0; i < dropArray.length; i++) {
-//     let Image = document.getElementById("Image")
-//     if (dropArray[i].name == diseasesDrop.value) {
-//         console.log(diseasesDrop.value)
-//         Image.src = dropArray[i].image
-//         console.log(Image.src)
-//     }
-// }
 
 function causesDrop() {
   document.getElementById("diseasesDropdown").classList.toggle("show");
@@ -408,11 +398,26 @@ function drawMap() {
 
 }
 
-
 let canvas = new p5(sketch2, "Map")
 // canvas.parent("Map")
 
 
+function showsInstructions(){
+   let comment = document.getElementById("comment")
+   comment.innerHTML = `Feeling lost again? Fine, I'll repeat the instructions. <br>
+   <br>First, choose the cause of death that you're interested in on the left. Then go 
+   the right to choose the victims' sex and race. Click on the "Generate" button 
+   to see the moving image.<br>
+   <br>
+   The amount and area that the white circles take represent the total deaths that this employee caused.
+   The amount and area of the blue circles on top represent deaths of people of the sex and race that you filtered.
+   The moving image only represents the rough idea, so don't focus on accuracy.<br>
+   <br>
+   What? Why does the sex options only have female and male? Uhhh perhaps our statistician died 
+   like 50 years ago so the system is behind.<br>
+   <br>
+   We have limited resources so don't ask me these questions!`
+}
 
 
 
